37a97bd7362522b02cf3d202b9b0670132425fb7
1132209300053 <1132209300053@nsl.ad.nec.co.jp>
2019-11-22 00:35:17 +0900

    2019-11-21 23:12

--- KnowledgeDB/settings.py
+++ KnowledgeDB/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'crw@l2-6pgk-dfhwi!e8g2b(!9)db38jtl=70f_0g-6lo0^gmb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-STATC_ROOT = os.path.join(BASE_DIR,'static')
+#STATC_ROOT = os.path.join(BASE_DIR,'static')
 #ALLOWED_HOSTS = ['localhost','127.0.0.1','.pythonanywhere.com']
 ALLOWED_HOSTS = ['*']
 
